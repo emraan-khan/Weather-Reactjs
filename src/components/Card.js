175c911d3e0f50f18240860411a8764ef12b6c7f
Imran Khan <imrankhan772244@gmail.com>
2024-06-12 21:50:03 +0530

    box

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = () => {
   return (
     <div className='card'>
       <img src={weather?.data?.current?.condition?.icon}/>
-      <h6> {weather?.data?.current?.condition?.text}</h6>
+      <h3> {weather?.data?.current?.condition?.text}</h3>
       <h2>{weather.data?.current.temp_c}°C</h2>
       <h5> {weather.data?.location?.name}, {weather.data?.location?.region}, {weather.data?.location?.country} </h5>
       <h5>Humidity : {weather.data?.current?.humidity}%</h5>
