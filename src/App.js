da9d59e9c612dd3edc9718f3b8ade404435d754a
Imran Khan <imrankhan772244@gmail.com>
2024-06-12 21:45:56 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 import Input from './components/Input'
 import Button from './components/Button'
 import { useWeather } from './context/Weather';
 import { useEffect } from 'react'
+import sunOff from "./assets/sunOff.png"
+import sunOn from "./assets/sunOn.png"
 
 
 const App = () => {
   const weather = useWeather();
+
+  const [sun, setSun] = useState(true)
 
   useEffect(()=>{
     weather.fetchCurrentUserLocation();
   },[])
 
   return (
-    <div className='App'>
+    <div className={`App ${sun? 'light':'dark'}`}>
+    <div className='title'>
       <h1>Weather Forecast</h1>
+      <img src={sun? sunOff: sunOn} className='icon' onClick={()=> setSun(!sun)}/>
+    </div>
       <Input />
       <Button onClick={weather.fetchData} value='Search' />
       <Card />
